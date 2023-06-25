f04ed6f62657126f13ef546bc8e16d121cc67dc2
Shritan Gunday <113409877+ShritanGunday@users.noreply.github.com>
2023-06-25 18:01:27 -0400

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { NavLinksComponent } from './nav-links/nav-links.component';
+import { CoursesComponent } from './courses/courses.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    NavLinksComponent,
+    CoursesComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    RouterModule.forRoot([
+      {path: 'coursework', component: CoursesComponent},
+      {path: '', component: HomeComponent},
+      {path: '**', component: PageNotFoundComponent}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
